5aef826f8ec15d5c79beb9475daba554f08ec7a3
jirryle <67206440+jirryle@users.noreply.github.com>
2024-03-07 03:09:15 -0600

    finished skeleton code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './App.css';
-import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
-      </Router>      
-    </>
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
   );
 }
 
