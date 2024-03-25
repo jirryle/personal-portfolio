7ae09196a98714115f6f9009aeb1a738d7f21fb9
jirryle <67206440+jirryle@users.noreply.github.com>
2024-03-25 01:21:28 -0500

    added personal info, work section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
+import Java from '../assets/java_icon.png';
+import Python from '../assets/python_icon.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
+import Pandas from '../assets/pandas_icon.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+import Heroku from '../assets/heroku_icon.png';
 
 const Skills = () => {
   return (
@@ -16,40 +15,40 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
-            <p className='py-4'> Theres are the technologies I've worked with</p>
+            <p className='py-4'> Theres are some of the recent technologies I've worked with!</p>
         </div>
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={HTML} alt="HTML icon"></img>
-                <p className='my-4'>HTML</p>
+                <img className='w-20 h-20 mx-auto' src={Java} alt="Java icon"></img>
+                <p className='my-4'>Java</p>
             </div>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={CSS} alt="HTML icon"></img>
-                <p className='my-4'>CSS</p>
+                <img className='w-20 h-20 mx-auto' src={Python} alt="Python icon"></img>
+                <p className='my-4'>Python</p>
             </div>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon"></img>
+                <img className='w-20 h-20 mx-auto' src={JavaScript} alt="HTML icon"></img>
                 <p className='my-4'>JavaScript</p>
             </div>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon"></img>
+                <img className='w-20 h-20 mx-auto' src={ReactImg} alt="HTML icon"></img>
                 <p className='my-4'>React</p>
             </div>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={GitHub} alt="HTML icon"></img>
+                <img className='w-20 h-20 mx-auto' src={GitHub} alt="HTML icon"></img>
                 <p className='my-4'>GitHub</p>
             </div>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={Node} alt="HTML icon"></img>
-                <p className='my-4'>Node</p>
+                <img className='w-20 h-20 mx-auto' src={Heroku} alt="Heroku icon"></img>
+                <p className='my-4'>Heroku</p>
             </div>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={FireBase} alt="HTML icon"></img>
-                <p className='my-4'>FireBase</p>
+                <img className='w-20 h-20 mx-auto' src={Pandas} alt="Pandas icon"></img>
+                <p className='my-4'>Pandas</p>
             </div>
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon"></img>
+                <img className='w-20 h-20 mx-auto' src={Tailwind} alt="HTML icon"></img>
                 <p className='my-4'>Tailwind</p>
             </div>
         </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import WorkImg from '../assets/workImg.jpeg';
+import React, { useState } from 'react';
+import ActivismImg from '../assets/activism.jpg';
 import realEstate from '../assets/realestate.jpg';
+import Popup from './Popup.jsx';
+import HackRiceVid from '../assets/hackricevid.mp4';
 
 const Work = () => {
+  const [activePopup, setActivePopup] = useState(null);
+
+  const togglePopup = (itemId) => {
+    setActivePopup(activePopup === itemId ? null : itemId);
+  };
+
   return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -10,154 +18,59 @@ const Work = () => {
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
             Work
           </p>
-          <p className='py-6'>// Check out some of my recent work</p>
+          <p className='py-6'>Check out some of my recent work!</p>
         </div>
 
-{/* Container */}
+        {/* Container */}
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
-            {/* Grid Item */}
+          {/* Item 1 */}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${ActivismImg})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
+            <div className='opacity-0 group-hover:opacity-100 flex flex-col items-center'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                BeActive
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
+                <button onClick={() => togglePopup('item1')} className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                  Learn More
+                </button>
               </div>
             </div>
-          </div>
+          </div> {/* Close Item 1 div properly */}
+
+          {/* Item 2 */}
           <div
             style={{ backgroundImage: `url(${realEstate})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
-            {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                Family Tree Maker
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-            {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-            {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
+                <button onClick={() => togglePopup('item2')} className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                  Coming soon!
+                </button>
               </div>
             </div>
           </div>
         </div>
+
+        {/* Popups */}
+        {activePopup === 'item1' && <Popup 
+          onClose={() => togglePopup(null)} 
+          videoSrc={HackRiceVid}
+          description="Lorem ipsum dolor sit amet..."
+        />}
+        
+        {activePopup === 'item2' && <Popup 
+          onClose={() => togglePopup(null)} 
+          videoSrc="path_to_another_video.mp4" // Placeholder, update with real video source
+          description="Coming soon!"
+        />}
       </div>
     </div>
   );
